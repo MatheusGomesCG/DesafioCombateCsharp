30d1f6cd2eb2f0d2c2c9420bafdaedf9743696a3
Matheus Gomes Farias Eusébio <matheusgomes153@gmail.com>
2025-03-12 12:08:09 -0300

    :tada: Commit inicial

---
+++ DesafioCombate/Champion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioCombate
+{
+    class Champion
+    {
+        public string Name { get; private set; }
+        public int Life { get; private set; }
+        public int Attack { get; private set; }
+        public int Armor { get; private set; }
+
+        public Champion(string name, int life, int damage, int armor)
+        {
+            Name = name;
+            Life = life;
+            Attack = damage;
+            Armor = armor;
+        }
+
+        public void TakeDamage (Champion other)
+        {
+            if (other.Life < Attack)
+            {
+                other.Life = 0;
+            }
+            else if (other.Armor >= Attack)
+            {
+                other.Life -= 1;
+            }
+            else
+            {
+                other.Life -= Attack - other.Armor;
+            }
+
+        }
+
+        public string Status()
+        {
+            if(Life == 0)
+            {
+                return $"{Name}: {Life} de vida (morreu)";
+            }
+            
+            return $"{Name}: {Life} de vida";
+        }
+
+    }
+}
---
+++ DesafioCombate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace DesafioCombate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int life, atack, def;
+           
+            Console.WriteLine("Digite os dados do primeiro campeão:");
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Vida inicial: ");
+            life = int.Parse(Console.ReadLine());
+            Console.Write("Ataque: ");
+            atack = int.Parse(Console.ReadLine());
+            Console.Write("Armadura: ");
+            def = int.Parse(Console.ReadLine());
+            Champion champOne = new Champion(name, life, atack, def);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite os dados do segundo campeão:");
+            Console.Write("Nome: ");
+            name = Console.ReadLine();
+            Console.Write("Vida inicial: ");
+            life = int.Parse(Console.ReadLine());
+            Console.Write("Ataque: ");
+            atack = int.Parse(Console.ReadLine());
+            Console.Write("Armadura: ");
+            def = int.Parse(Console.ReadLine());
+            Champion champTwo = new Champion(name, life, atack, def);
+
+
+            Console.WriteLine();
+            Console.Write("Quantos turnos você deseja executar? ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for(int i = 1; i <= choice; i++)
+            {
+                Console.WriteLine($"Resultado do turno {i}");
+                champOne.TakeDamage(champTwo);
+                champTwo.TakeDamage(champOne);
+                Console.WriteLine(champOne.Status());
+                Console.WriteLine(champTwo.Status());
+
+                if(champOne.Life == 0 || champTwo.Life == 0){
+                    Console.WriteLine();
+                    break;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("FIM DO COMBATE");
+        }
+    }
+}
